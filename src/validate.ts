744a19bb68dfd812a0edb98a81def2ca348b4f89
laiwei <184440579@qq.com>
2018-11-27 23:15:46 +0800

    validate.ts

--- src/validate.ts
+++ src/validate.ts
@@ -5,145 +5,167 @@
  * @Last Modified by: Lingluo
  * @Last Modified time: 2018-11-27 18:22:48
  */
-var validate = function() {
-	var s = ['require', 'len','minLen','maxLen','pattern', 'test'];
-	var len = s.length;
 
-	function trim(a){
-		return a.toString().replace(/^\s+|\s+$/g, '');
+/**
+* 
+* rules 规则格式
+* rules ={
+*  nick: {
+* 		require: [true,'nick必填'],
+*		type:['email','不是邮箱']
+*		len:[6,"长度不正确"],
+*		minLen:[4,"长度最小四位"],
+*		maxLen:[6,"长度最大6位"],
+*		regExp:[/\d{11}/,'格式错误'],
+*      func:[function(field,fields){return true},'验证未通过']
+*  } 
+* }
+* 
+*  
+* 需要验证的表单字段
+* data = {
+* 		nick:'username',
+* 		age:26
+* }
+* 
+* 
+* */
+
+type IRule = [boolean | number | RegExp | Function | string, string];
+
+interface IRules {
+	require?: IRule,
+	len?: IRule,
+	minLen?: IRule,
+	maxLen?: IRule,
+	regExp?: IRule,
+	func?: IRule
+}
+
+interface IValidateRule {
+	[x: string]: IRules
+}
+interface IValidateResultItem {
+	field: string,
+	msg: any
+};
+interface IValidateResult {
+	state: boolean,
+	msg?: string,
+	fields?: IValidateResultItem[]
+}
+const type = (object): string => {
+	return Object.prototype.toString
+		.call(object)
+		.replace(/(^\[\w+\s+)|(\]$)/g, '')
+		.toLowerCase();
+};
+
+const isEmpty = (n: any): boolean => {
+	if (n === undefined || n === null) {
+		return true;
 	}
-
-    var validator = {
-        require: function(a, bool){
-            if (bool === true)
-            {
-                return (typeof a !== 'undefined') && (a !== null) && !!trim(a);
-            }
-            else if (bool === false)
-            {
-                return true;
-            }
-        },
-        len: function(a, len){
-            return (typeof a !== 'undefined') && (a !== null) && trim(a).length === len;
-        },
-        minLen:function(a,len){
-        	return (typeof a !== 'undefined') && (a !== null) && trim(a).length >= len;
-        },
-        maxLen:function(a,len){
-        	return (typeof a !== 'undefined') && (a !== null) && trim(a).length <= len;
-        },
-        pattern: function(a, pattern){
-            return pattern.test(a);
-        },
-        test: function(a, fn, c) {
-        	var result = fn(a, c);
-            return typeof result !== 'undefined' ? result : true;
-        }
-    };
-
-    return function(rule, data){
-	        if ('object' === typeof data && 'object' === typeof rule)
-	        {
-	            for (var i in rule)
-	            {
-	                if (rule.hasOwnProperty(i))
-	                {
-	                    var item = rule[i];
-	      				for (var j = 0 ; j < len; j++)
-	                    {
-	                        if (item.hasOwnProperty(s[j]))
-	                        {
-	                         
-	                            if(s[j] === "test")
-	                            {
-	                            	if(item[s[j]] instanceof Array)
-	                            	{
-	                            		for(var k = 0 ,klen = item[s[j]].length ; k < klen ; k++)
-	                            		{
-	                            			var result =  validator[s[j]](data[i], item[s[j]][k].val,data);
-	                            			if(!result)
-	                            			{
-	                            				return {status:false,msg:item[s[j]][k].msg || ''};
-	                            			}
-	                            		}
-	                            	}
-	                            	else
-	                            	{
-	                            		var result =  validator[s[j]](data[i], item[s[j]].val,data);
-	                            	}
-	                            	
-	                            }
-	                            else
-	                            {
-	                            	var result =  validator[s[j]](data[i], item[s[j]].val);
-	                            }
-
-	                            if(result && s[j] === 'require' && (data[i] === undefined || data[i] === null || trim(data[i]) === ''))
-	                            {
-	                            	break;
-	                            }
-
-	                            if (!result) 
-	                            {
-	                                return {status: false,msg: item[s[j]].msg || ''};
-	                            }
-	                        }
-	                    }
-	                }
-	            }
-	            return {status: true};
-	        }
-	        else
-	        {
-	            throw new Error('params type error');
-	        }
-	    }
-}();
-
-
-/*
-*
-*rule 规则格式
-rules ={
-    "nick": {
-        "require": {
-            "val": true, 
-            "msg": "名称不能为空"
-        }
-    }, 
-    "email": {
-        "require": {
-            "val": false, 
-            "msg": "邮箱不能为空"
-        }, 
-        "pattern": {
-            "val": new RegExp(/^[a-zA-Z0-9]+[a-zA-Z0-9\.-_]*@[a-zA-Z0-9]+\.[\w]+$/i), 
-            "msg": "邮箱格式不正确"
-        }
-    }, 
-    "content": {
-        "require": {
-            "val": true, 
-            "msg": "内容不能为空"
-        }
-    }, 
-    "verify": {
-        "require": {
-            "val": true, 
-            "msg": "验证码不能为空"
-        }, 
-        "len": {
-            "val": 4, 
-            "msg": "验证码位数不正确"
-        }
-    }
+	switch (type(n)) {
+		case 'string':
+			return n.trim() === '';
+		case 'array':
+			return !n.length;
+		case 'object':
+			return !Object.keys(n).length;
+		case 'number':
+			return isNaN(n);
+	}
+	return false;
 }
-*要验证的data格式
-data = {
-	email:'xxx',
-	nick:'xxx',
-	content:'xxx'
+
+const TYPES = {
+	moblie: /^1[3456789]\d{9}$/,
+	number: /^\d+(\.\d+)?$/,
+	money: /^\d+(\.\d{2})?$/,
+	int: /^\d+/,
+	email: /^[\w\d]+[\d\w_]+@[\w\d]+(\.\w+)+$/i
 }
-*
-*/
+
+const VALIDATORS = {
+	require(a: any, bool: boolean): boolean {
+		return bool ? !isEmpty(a) : true;
+	},
+	type(a: any, b: RegExp): boolean {
+		return b.test(a);
+	},
+	len(a: any, len: number): boolean {
+		return !isEmpty(a) && a.trim().length === len;
+	},
+	minLen(a: any, len: number): boolean {
+		return !isEmpty(a) && a.trim().length >= len;
+	},
+	maxLen(a: any, len: number): boolean {
+		return !isEmpty(a) && a.trim().length <= len;
+	},
+	regExp(a: any, regExp: RegExp): boolean {
+		return regExp.test(a);
+	},
+	func(a: any, fn: Function, c: any): boolean {
+		const result = fn(a, c);
+		return result === undefined ? true : result;
+	}
+};
+
+const RULE_NAMES: string[] = ['require', 'type', 'len', 'minLen', 'maxLen', 'regExp', 'func'];
+const RULE_LEN = RULE_NAMES.length;
+
+declare global{
+	interface Window{
+		valid:any
+	}
+}
+
+export const valid = (rules: IValidateRule, data: { [x: string]: any }): IValidateResult => {
+
+	let ret: IValidateResult = {
+		state: true,
+		msg: '',
+		fields: []
+	};
+
+	for (let key in rules) {
+		if (rules.hasOwnProperty(key)) {
+			let rule: IRules = rules[key];
+			for (let i = 0; i < RULE_LEN; i++) {
+				let ruleName: string = RULE_NAMES[i];
+				if (rule.hasOwnProperty(ruleName)) {
+					let ruleItem: IRule = rule[ruleName];
+					let func: Function = VALIDATORS[ruleName];
+					let ruleVal = ruleItem[0],
+						ruleMsg: string = ruleItem[1];
+					let args = [data[key], ruleVal];
+					if (ruleName === 'func') {
+						args.push(data);
+					}
+					if (ruleName === 'type') {
+						args = [data[key], TYPES[<string>ruleVal]];
+					}
+					let is: boolean = func(...args);
+					if (!is) {
+						ret.state = false;
+						if (!ret.msg) {
+							ret.msg = ruleMsg;
+						}
+						let field: IValidateResultItem = {
+							field: key,
+							msg: ruleMsg
+						}
+						ret.fields.push(field);
+						break;
+					}
+
+				}
+			}
+		}
+	}
+	return ret;
+}
+
+
+window.valid = valid;
+
